190f46728f47e28a8cc7524534e73cb1f7aa83b0
theophilus-alamu <theophilus.alamu8@gmail.com>
2026-01-15 11:53:52 +0100

    clean up

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
-  //app.setGlobalPrefix('api');
-
   if (process.env.VERCEL) {
     await app.init();
     return;
