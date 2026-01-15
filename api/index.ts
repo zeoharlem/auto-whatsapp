b843dd31d3dd0c2c384add20317e45d5e08a1f08
theophilus-alamu <theophilus.alamu8@gmail.com>
2026-01-15 11:48:02 +0100

    fix error 404 on vercel deployment

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
+/*
 let server;
-
 export default async function handler(req, res) {
   if (!server) {
     const app = await NestFactory.create(AppModule);
@@ -12,3 +14,15 @@ export default async function handler(req, res) {
 
   return server(req, res);
 }
+*/
+const server = express();
+
+export default async (req, res) => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  // Important: If your frontend is on a different domain
+  app.enableCors();
+
+  await app.init();
+  server(req, res);
+};
